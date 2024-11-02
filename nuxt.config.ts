a53108f320b66d75991f355b8bfafdbc051eee6e
Dimples_YJ <2890841438@qq.com>
2024-11-03 02:24:55 +0800

    feat: add mobile adapt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  compatibilityDate: '2024-11-03',
   css: ['@unocss/reset/tailwind.css', './assets/css/main.css'],
   modules: ['@unocss/nuxt', '@nuxt/eslint', '@nuxt/content', '@nuxt/image'],
 
   app: {
     head: {
-      title: 'DimplesY\'s Site'
+      title: 'DimplesY\'s Site',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
     }
   },
-  compatibilityDate: '2024-11-03'
 })
