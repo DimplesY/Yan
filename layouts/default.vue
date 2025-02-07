d93c153d26862574558bfa8a102c92e7fd7b195e
Dimples_YJ <2890841438@qq.com>
2025-02-08 00:00:59 +0800

    fix: 修改样式

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="min-h-screen flex flex-col">
-    <header class="flex w-full text-foreground justify-between items-center max-w-7xl mx-auto py-6 px-2 border-b border-border">
-      <NuxtLink to="/">DimplesY</NuxtLink>
-      
-      <NuxtLink class="flex items-center justify-center gap-x-2 px-2 rounded transition-all duration-150 hover:bg-muted"  to="/collection">
-        <div class="icon-[carbon--book]"/>
-        <span>Collection</span>
-      </NuxtLink>
-    </header>
-
+  <div class="h-screen flex flex-col">
     <slot />
   </div>
 </template>
