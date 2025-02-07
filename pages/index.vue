467aec7b8b71c9e89ce9cedcfd5859ad7a76cd66
Dimples_YJ <2890841438@qq.com>
2025-02-08 01:06:04 +0800

    style: 修改样式

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="min-h-screen container max-w-7xl mx-auto text-foreground py-20 box-border px-5 md:px-10 xl:px-20 border-x border-solid border-zinc-100">
+  <div class="min-h-screen container max-w-7xl mx-auto text-foreground py-10 xl:py-20 box-border px-5 md:px-10 xl:px-20 border-x border-solid border-zinc-100">
 
-    <NuxtImg v-motion-fade src="https://avatars.githubusercontent.com/u/51285767" class="rounded-full block h-20 w-20 border shadow" format="webp" placeholder alt="DimplesY" />
+    <NuxtImg v-motion-fade src="https://avatars.githubusercontent.com/u/51285767" class="rounded-full block h-20 mx-auto md:mx-0 w-20 border shadow" format="webp" placeholder alt="DimplesY" />
 
     <div class="h-10 mt-10">
       <ClientOnly>
@@ -9,7 +9,7 @@
           :delay="0.2"
           :duration="0.75"
         >
-          <h1 class="text-xl md:text-2xl lg:text-3xl font-bold">Hey, I'm DimplesY 👋</h1>
+          <h1 class="text-xl md:text-2xl lg:text-3xl">Hey, I'm DimplesY 👋</h1>
         </BlurReveal>
       </ClientOnly>
     </div>
@@ -25,7 +25,7 @@
     </div>
 
 
-    <div class="flex flex-col gap-10">
+    <div class="flex flex-col gap-10 mt-10">
       <div>
         <h2 class="text-2xl font-bold">Skills</h2>
         <ul class="grid grid-cols-2 lg:grid-cols-3 gap-2 mt-2">
